bc0da28d09910c1bb23791e88a4ecfb939052062
jinyus <starboymc4@gmail.com>
2021-12-23 15:28:51 -0500

    refactor split function

--- Split.fs
+++ Split.fs
@@ -4,82 +4,63 @@ open dirsplitter.util
 open System.IO
 open System.Text.RegularExpressions
 
-type SplitResult =
-    { tracker: Map<int, int64>
-      currentPart: int
-      filesMoved: int
-      failedOps: int }
 
-    static member Default =
-        { tracker = Map.empty
-          currentPart = 1
-          filesMoved = 0
-          failedOps = 0 }
+let increment key byAmount (map: Map<int, int64>) = map |> incrementMap key byAmount
 
-    member this.increment key byAmount =
-        { this with tracker = this.tracker |> incrementMap key byAmount }
-
-
-let moveFile (state: SplitResult) (filepath: string) dir maxBytes : SplitResult =
-    let file = FileInfo(filepath)
-    let size = file.Length
-    let mutable currentPart = state.currentPart
-    let mutable filesMoved = state.filesMoved
-    let mutable failedOps = state.failedOps
-
-    let mutable currentPartSize = state.tracker |> findOrZero currentPart
-
-
-    let decrementOnFailure =
-        if currentPartSize > 0
-           && (currentPartSize + size) > maxBytes then
-            currentPart <- currentPart + 1
-            currentPartSize <- 0
-            true
-        else
-            false
-
-
-    let res = state.increment currentPart size
-
-    let dest =
-        Path.Join [| dir
-                     $"part{currentPart}"
-                     filepath.Replace(dir, "") |]
-
-    try
-        let parentDir = (Directory.GetParent dest).FullName
-        Directory.CreateDirectory parentDir |> ignore
-
-        file.MoveTo dest
-        filesMoved <- filesMoved + 1
-    with
-    | e ->
-        printfn "%s" e.Message
-        failedOps <- failedOps + 1
-
-        currentPart <-
-            if decrementOnFailure then
-                currentPart - 1
-            else
-                currentPart
-
-    { res with
-        currentPart = currentPart
-        filesMoved = filesMoved
-        failedOps = failedOps }
-
-
+let printTrackerMap (map: Map<int, int64>) =
+    for KeyValue (key, value) in map do
+        printfn "part%i : %iMB" key (value / int64 (1000.0 ** 2))
 
 let splitDir (dir, maxBytes: int64, prefix) =
+    let mutable tracker: Map<int, int64> = Map.empty
+    let mutable currentPart = 1
+    let mutable filesMoved = 0
+    let mutable failedOps = 0
     let sep = Path.DirectorySeparatorChar
 
     // used to exclude files moved to newly created partDirs from getAllFiles
     let partDirRe = $"{dir}{sep}part\d+{sep}.*"
 
-    let result =
-        getAllFiles dir
-        |> Seq.where (fun s -> not (Regex.IsMatch(s, partDirRe)))
-        |> Seq.fold (fun r s -> moveFile r s dir maxBytes) SplitResult.Default
+    getAllFiles dir
+    |> Seq.where (fun s -> not (Regex.IsMatch(s, partDirRe)))
+    |> Seq.iter (fun filepath ->
+        let file = FileInfo(filepath)
+        let size = file.Length
+        let mutable currentPartSize = tracker |> findOrZero currentPart
 
-    printfn "result %O %i" "result" maxBytes
+        let decrementOnFailure =
+            if currentPartSize > 0
+               && (currentPartSize + size) > maxBytes then
+                currentPart <- currentPart + 1
+                currentPartSize <- 0
+                true
+            else
+                false
+
+        tracker <- tracker |> increment currentPart size
+
+        let dest =
+            Path.Join [| dir
+                         $"part{currentPart}"
+                         filepath.Replace(dir, "") |]
+
+        let parentDir = (Directory.GetParent dest).FullName
+
+        try
+            Directory.CreateDirectory parentDir |> ignore
+            file.MoveTo dest
+            filesMoved <- filesMoved + 1
+        with
+        | e ->
+            printfn "%s" e.Message
+            failedOps <- failedOps + 1
+
+            currentPart <-
+                if decrementOnFailure then
+                    currentPart - 1
+                else
+                    currentPart)
+
+
+    printfn "Results:\nFiles Moved : %i \nFailed Operations : %i" filesMoved failedOps
+    tracker |> printTrackerMap
