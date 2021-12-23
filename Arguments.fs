fbb85cac1a87e952422cb83bb7d613a898a25857
jinyus <starboymc4@gmail.com>
2021-12-23 01:27:30 -0500

    remove unused types

--- Arguments.fs
+++ Arguments.fs
@@ -2,46 +2,6 @@ namespace dirsplitter
 
 open Argu
 
-[<CliPrefix(CliPrefix.Dash)>]
-type CleanArgs =
-    | D
-    | F
-    | X
-
-    interface IArgParserTemplate with
-        member this.Usage =
-            match this with
-            | D -> "Remove untracked directories in addition to untracked files"
-            | F -> "Git clean will refuse to delete files or directories unless given -f."
-            | X -> "Remove only files ignored by Git."
-
-type CommitArgs =
-    | Amend
-    | [<AltCommandLine("-p")>] Patch
-    | [<AltCommandLine("-m")>] Message of msg: string
-
-    interface IArgParserTemplate with
-        member this.Usage =
-            match this with
-            | Amend -> "Replace the tip of the current branch by creating a new commit."
-            | Patch -> "Use the interactive patch selection interface to chose which changes to commit."
-            | Message _ -> "Use the given <msg> as the commit message. "
-
-type GitArgs =
-    | Version
-    | [<AltCommandLine("-v")>] Verbose
-    | [<CliPrefix(CliPrefix.None)>] Clean of ParseResults<CleanArgs>
-    | [<CliPrefix(CliPrefix.None)>] Commit of ParseResults<CommitArgs>
-
-    interface IArgParserTemplate with
-        member this.Usage =
-            match this with
-            | Version -> "Prints the Git suite version that the git program came from."
-            | Verbose -> "Print a lot of output to stdout."
-            | Clean _ -> "Remove untracked files from the working tree."
-            | Commit _ -> "Record changes to the repository."
-
-
 type ActionArgs =
     | [<AltCommandLine("-d")>] Dir of directory: string
     | [<AltCommandLine("-m")>] Max of maxGB: float
