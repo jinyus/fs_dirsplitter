7b9bed118312072e092608545fd4447abc79a8d9
jinyus <starboymc4@gmail.com>
2021-12-23 06:29:23 -0500

    create util functions

---
+++ Util.fs
@@ -0,0 +1,25 @@
+module dirsplitter.util
+
+open System.IO
+
+let rec getAllFiles dir =
+    seq {
+        yield! Directory.EnumerateFiles(dir)
+
+        for d in Directory.EnumerateDirectories(dir) do
+            yield! getAllFiles d
+    }
+
+let incrementOption (o: option<int64>) (byAmount: int64) =
+    match o with
+    | Some value -> Some(value + byAmount)
+    | None -> Some byAmount
+
+let incrementMap (key: int) (byAmount: int64) (map: Map<int, int64>) =
+    map
+    |> Map.change key (fun o -> incrementOption o byAmount)
+
+let findOrZero key (map: Map<int, int64>) =
+    match map.TryFind key with
+    | Some v -> v
+    | None -> 0
